845918898c1ab1b4a159fe6397634f41493abcb6
--replace-all <aghedopatrick@gmail.com>
2022-10-26 22:48:38 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React, { Components } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./Navigation";
-import Home from './components/Home';
-import Users from './components/Users';
-
+import PageNotFound from "./components/404page";
+import Home from "./components/Home";
+import Users from "./components/Users";
 import "./style.css";
-
-// function Home() {
-//   return (
-//     <section className="home-container">
-//       <h1>Home Page</h1>
-//       <p>This is the Home Page</p>
-//       <Navigation />
-//     </section>
-//   );
-// }
-
-// function Users() {
-//   return (
-//     <section className="users-container">
-//       <h1>Users</h1>
-//       <p>This is the Users Page</p>
-//       <Navigation />
-//     </section>
-//   );
-// }
 
 function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
       </Routes>
