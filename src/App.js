f18aa1f73f9800da34bea313fc0fb657492fa401
--replace-all <aghedopatrick@gmail.com>
2022-10-26 19:38:42 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Components } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./Navigation";
+import Home from './components/Home';
+import Users from './components/Users';
+
+import "./style.css";
+
+// function Home() {
+//   return (
+//     <section className="home-container">
+//       <h1>Home Page</h1>
+//       <p>This is the Home Page</p>
+//       <Navigation />
+//     </section>
+//   );
+// }
+
+// function Users() {
+//   return (
+//     <section className="users-container">
+//       <h1>Users</h1>
+//       <p>This is the Users Page</p>
+//       <Navigation />
+//     </section>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
